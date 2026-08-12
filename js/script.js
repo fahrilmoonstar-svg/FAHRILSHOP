411bdc1a90ec8358bbdd3191fb9c6bb05f422fa8
fahrilmoonstar-svg <fahrilmoonstar@gmail.com>
2026-08-12 15:11:40 +0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// ===== KODE FOR LOOP (DARI DAHULU) =====
+const fruits = [
+    "Apple",
+    "Banana",
+    "Mango",
+    "Orange",
+    "Grape",
+    "Watermelon"
+];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+
+// ===== KODE WHILE LOOP (YANG BARU) =====
+const fruits2 = [
+    "Apple",
+    "Banana",
+    "Mango",
+    "Orange"
+];
+
+let j = 0;
+
+while (j < fruits2.length) {
+    console.log(fruits2[j]);
+    j++;
+}
